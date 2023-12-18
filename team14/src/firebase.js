4bd655e0c514698cd45b6e4ab37bfcbeca8be271
Hadi Malik <Amir@Hadis-MacBook-Pro.local>
2023-12-18 21:22:52 +0200

    initialize firebase database

--- team14/src/firebase.js
+++ team14/src/firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
-import {Storage} from "./firebase";
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCnbgcE6lHib_ZUgAJpH7CWXvzlT9tEOtQ",
   authDomain: "team14-122.firebaseapp.com",
+  databaseURL: "https://team14-122-default-rtdb.firebaseio.com",
   projectId: "team14-122",
   storageBucket: "team14-122.appspot.com",
   messagingSenderId: "821168664069",
   appId: "1:821168664069:web:8feaf24b97c51955cd366c"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const firebase = Storage(app);
+const database = getDatabase(app);
+export { database };
