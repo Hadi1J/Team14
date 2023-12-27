ce55bdea5b0eef92777213aa70864f6df2499bd7
Hadi Malik <Amir@Hadis-MacBook-Pro.local>
2023-12-27 18:53:46 +0200

    edited

--- team14/src/App.js
+++ team14/src/App.js
@@ -1,32 +1,25 @@
-import React, { useState } from 'react';
+import React from "react";
 import PostManager from "./components/PostManager";
-import GroupFeed from "./components/group";
-import About from "./components/About";
-import "./App.css";
-import Navbar from "./components/Navbar";
-import MainContent from './components/MainContent';
-
+import Group from "./components/Group/Group";
+import About from "./components/About/About";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
-  const [likeCount, setLikeCount] = useState(0);
-
-    const addLike = () => {
-        setLikeCount(likeCount + 1);
-    };
   return (
     <div className="App">
-    <Header><Navbar/><MainContent/></Header>
-      <div className="container">
-        <div className="row g-4">
-          <GroupFeed />
-        </div>
-        <div className="row g-4">
-          <div class="col-lg-8 vstack gap-4">
-            <PostManager />
+      <Navbar />
+      <div className="main">
+        <div className="container">
+          <div className="row g-4">
+            <div class="col-md-8 col-lg-9 vstack gap-4">
+              <Group />
+            </div>
           </div>
-          <div className="col-lg-4">
-            <div className="Aboutgrid">
+          <div className="row g-4">
+            <div class="col-lg-8 vstack gap-4">
+              <PostManager />
+            </div>
+            <div className="col-lg-4">
               <About />
             </div>
           </div>
