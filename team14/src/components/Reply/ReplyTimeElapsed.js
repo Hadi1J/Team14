3b5c48a91184f152c92f29ed0c7da750bd3d2e12
Your Name <you@example.com>
2023-12-28 16:30:08 +0200

    theendd

--- team14/src/components/Reply/ReplyTimeElapsed.js
+++ team14/src/components/Reply/ReplyTimeElapsed.js
@@ -3,33 +3,37 @@ function ReplyTimeElapsed(ReplyDate) {
     var currentTime = now.getTime();
     var secondsElapsed = Math.floor((currentTime - ReplyDate) / 1000);
 
+  
     if (secondsElapsed < 60) {
       return secondsElapsed + " sec";
-    }
-     else 
-     {
+    } else {
       var minutesElapsed = Math.floor(secondsElapsed / 60);
-      if (minutesElapsed < 60) 
-      {
+      if (minutesElapsed < 60) {
         return minutesElapsed + " min";
-      } 
-      else 
-      {
+      } else {
         var hoursElapsed = Math.floor(minutesElapsed / 60);
-
-
-        if(hoursElapsed<24)
-
-        {return hoursElapsed + " hour";}
-
-        else{
-
-          var daysElapsed =Math.floor(hoursElapsed / 24)
-          return daysElapsed + " day ";
+  
+        if (hoursElapsed < 24) {
+          return hoursElapsed + " hour";
+        } else {
+          var daysElapsed = Math.floor(hoursElapsed / 24);
+  
+          if (daysElapsed < 7) {
+            return daysElapsed + (daysElapsed === 1 ? " day" : " days");
+          } else if (daysElapsed < 30) {
+            var weeksElapsed = Math.floor(daysElapsed / 7);
+            return weeksElapsed + (weeksElapsed === 1 ? " week" : " weeks");
+          } else if (daysElapsed < 365) {
+            var monthsElapsed = Math.floor(daysElapsed / 30);
+            return monthsElapsed + (monthsElapsed === 1 ? " month" : " months");
+          } else {
+            var yearsElapsed = Math.floor(daysElapsed / 365);
+            return yearsElapsed + (yearsElapsed === 1 ? " year" : " years");
+          }
         }
-
       }
     }
   }
+  
 
   export default ReplyTimeElapsed;
